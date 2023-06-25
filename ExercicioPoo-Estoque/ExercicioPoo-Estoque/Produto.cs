ad601502fb0f34971572a7549b527a9a9cc53655
Gabu <44330034+g4bh@users.noreply.github.com>
2023-06-25 19:50:24 -0300

    Atividade de fixação - POO: Lista 2
    Na atividade foram postos em prática os conhecimentos de classes, atributos e métodos, ensinados no curso. Utilizei também switch case, mas com conhecimentos anteriores.

---
+++ ExercicioPoo-Estoque/ExercicioPoo-Estoque/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPoo_Estoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+
+        public double valorTotalEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarQuantidade(int quantidade) {
+            Quantidade += quantidade;
+        }
+
+
+        public void RemoverQuantidade(int quantidade) { 
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade 
+            + " unidades \nTOTAL: R$ " + valorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExercicioPoo-Estoque/ExercicioPoo-Estoque/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ExercicioPoo_Estoque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto();
+            int opcao;
+
+            do
+            {
+                Console.WriteLine("Escolha uma opção:\n1 - Entrar com os dados do produto \n2 - Adicionar produtos \n3 - Remover produtos\n4 - Sair\n");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao) 
+                {
+                    case 1:
+                        Console.WriteLine("\nEntre os dados dos produtos: ");
+                        Console.Write("Nome: ");
+                        p.Nome = Console.ReadLine();
+
+                        Console.Write("Preço: ");
+                        p.Preco = double.Parse(Console.ReadLine());
+
+                        Console.Write("Quantidade no estoque: ");
+                        p.Quantidade = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("\nDados dos produtos: " + p);
+                        break;
+                        
+                    case 2:
+                        Console.Write("\nDigite o número de produtos que deseja adicionar:");
+                        int qte = int.Parse(Console.ReadLine());
+                        p.AdicionarQuantidade(qte);
+                        Console.WriteLine("\nDados atualizados: " + p);
+                        break;
+
+                    case 3:
+                        Console.Write("\nDigite o número de produtos que deseja excluir:");
+                        int qtExcluir = int.Parse(Console.ReadLine());
+                        p.RemoverQuantidade(qtExcluir);
+                        Console.WriteLine("Dados atualizados: " + p);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nPROGRAMA ENCERRADO!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inálida! Tente novamente!\n");
+                        break;
+                };
+            } while (opcao != 4);
+        }
+    }
+}
