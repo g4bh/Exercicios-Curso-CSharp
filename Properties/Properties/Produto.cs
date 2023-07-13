998a05fc48070b2b2ec2710204152ced081624e1
Gabu <44330034+g4bh@users.noreply.github.com>
2023-07-12 21:30:36 -0300

    Properties
    Utilizando as referências da palavra This, método de Encapsulamento Get e Set e Properties

---
+++ Properties/Properties/Produto.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Produto
+    {
+        /*public string Nome;
+        public double Preco;
+        public int Quantidade;*/
+
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        /*Uso da palavra THIS*/
+
+        /*Referenciando outro construtor*/
+
+        public Produto() {
+            _quantidade = 0;
+        }
+
+        public Produto(string nome, double preco) : this() {
+            _nome = nome;
+            _preco = preco;
+        }
+
+        public Produto(string nome, double preco, int quantidade) : this(nome, preco)
+        {
+            _quantidade = quantidade;
+        }
+
+        /*---------------------------------*/
+
+
+        /*Encapsulamento*/
+        /*Uso de Get e Set:*/
+
+        /*public string GetNome() { 
+             return _nome;
+        }
+
+        public void SetNome(string nome) {
+            if (nome != null & nome.Length > 1) {
+                _nome = nome;
+            };
+        }*/
+
+        /*------------------------------------*/
+
+
+        /*Properties*/
+        
+
+        public string Nome {
+            get { return _nome; }
+            set {
+                if (value != null & value.Length > 1)
+                {
+                    _nome = value;
+                };
+            }
+        }
+
+        public double Preco {
+            get { return _preco; } 
+        }
+
+        public int Quantidade {
+            get { return _quantidade; }
+        }
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + _preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + _quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("Tv", 500.00, 1);
+
+            p.Nome = "Tv HD 4K";
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+
+            
+
+            /*
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            
+            Console.WriteLine("Dados atualizados: " + p);*/
+        }
+    }
+}
