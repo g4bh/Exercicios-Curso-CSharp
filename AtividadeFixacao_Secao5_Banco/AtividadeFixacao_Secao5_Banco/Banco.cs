52387c1ecdbee707e7b3b0a180869d42c7edb2bc
Gabu <44330034+g4bh@users.noreply.github.com>
2023-07-16 21:06:58 -0300

    Atividade de Fixacao - Secao 5
    Atividade de fixação da seção 5, utilizando vários conceitos ensinados na seção, como: construtores, uso da palavra 'this', sobrecarga, properties e encapsulamento

---
+++ AtividadeFixacao_Secao5_Banco/AtividadeFixacao_Secao5_Banco/Banco.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeFixacao_Secao5_Banco
+{
+    class Banco
+    {
+        public int Conta { get ; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Banco(int conta, string titular){
+            Conta = conta;
+            Titular = titular;
+        }
+
+        public Banco(int conta, string titular, double saldo) : this (conta, titular)
+        {
+            Saldo = saldo;
+        }
+
+        
+        
+        public void DepositoConta(double valorDeposito) {
+            Saldo = Saldo + valorDeposito;
+        }
+
+        public void SaqueConta(double valorSaque) {
+            int taxa = 5;
+            Saldo = (Saldo - valorSaque) - taxa;
+        }
+
+        public override string ToString() { 
+            return "Conta " + Conta + ", Titular: " + Titular + ", Saldo: R$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ AtividadeFixacao_Secao5_Banco/AtividadeFixacao_Secao5_Banco/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+
+namespace AtividadeFixacao_Secao5_Banco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco b;
+
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            
+            Console.Write("Haverá depósito inicial? (s/n) ");
+            char condicao = char.Parse(Console.ReadLine());
+            if (condicao == 's' || condicao == 'S')
+            {
+                Console.WriteLine("");
+                Console.Write("Entre com o valor do deposito:");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                b = new Banco(numero, titular, depositoInicial);
+            }
+            else { 
+                b = new Banco(numero, titular);
+            }
+
+            Console.WriteLine("Dados da conta: " + b);
+
+
+            Console.WriteLine("");
+            Console.Write("Entre um valor para depósito: ");
+            double valor = double.Parse(Console.ReadLine());
+            b.DepositoConta(valor);
+
+            Console.WriteLine("Dados da conta: " + b);
+
+
+
+            Console.WriteLine("");
+            Console.Write("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine());
+            b.SaqueConta(valor);
+
+            Console.WriteLine("Dados da conta: " + b);
+        }
+    }
+}
